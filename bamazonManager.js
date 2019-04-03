e645150a63763aa2c49870efa12c3e0116d18684
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-04-03 17:35:56 -0500

    allowed user to enter new items into their database

--- bamazonManager.js
+++ bamazonManager.js
@@ -13,21 +13,21 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
 
     menu();
 
 })
 
-function menu(){
+function menu() {
     console.log("\n");
     inquirer.prompt({
         name: "choice",
         type: "list",
         message: "Hello, what would you like to do?",
         choices: ["View Products", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"]
-    }).then(function(answer){
+    }).then(function (answer) {
         if (answer.choice === "View Products") {
             viewProducts();
         }
@@ -38,7 +38,7 @@ function menu(){
             addToInventory();
         }
         else if (answer.choice === "Add New Product") {
-            // addNewProduct();
+            addNewProduct();
         }
         else if (answer.choice === "Exit") {
             connection.end();
@@ -49,7 +49,7 @@ function menu(){
 
 function viewProducts() {
     console.log("\nDisplaying all products...\n");
-    connection.query("SELECT * FROM products", function(err, res){
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
 
         console.table(res);
@@ -59,7 +59,7 @@ function viewProducts() {
 
 function viewLowInventory() {
     console.log("\nDisplaying low inventory...\n");
-    connection.query("SELECT * FROM products WHERE stock_quantity < 5", function(err, res) {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 5", function (err, res) {
         if (err) throw err;
 
         console.table(res);
@@ -69,24 +69,24 @@ function viewLowInventory() {
 }
 
 function addToInventory() {
-    connection.query("SELECT * FROM products", function(err, results) {
+    connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
 
         inquirer.prompt([
             {
                 name: "id",
                 message: "Enter the ID number of the item you wish to add to.",
-                validate: function(value) {
+                validate: function (value) {
                     if (isNaN(value) === false) {
                         return true;
                     }
-                    return false; 
+                    return false;
                 }
             },
             {
                 name: "quantity",
                 message: "How many units would you like to add?",
-                validate: function(value) {
+                validate: function (value) {
                     if (isNaN(value) === false) {
                         return true;
                     }
@@ -94,36 +94,93 @@ function addToInventory() {
                 }
             }
         ])
-        .then(function(answer){
-            console.log("\n");
-            var chosenItem;
-            for (var i = 0; i < results.length; i++){
-                if (results[i].id === parseInt(answer.id)) {
-                    chosenItem = results[i];
-                }
-            }
-
-            var newQuantity = chosenItem.stock_quantity + parseInt(answer.quantity);
-            connection.query(
-                "UPDATE products SET ? WHERE ?",
-                [
-                    {
-                        stock_quantity: newQuantity
-                    },
-                    {
-                        id: chosenItem.id
+            .then(function (answer) {
+                console.log("\n");
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].id === parseInt(answer.id)) {
+                        chosenItem = results[i];
                     }
-                ],
-                function(error) {
-                    if (error) throw error;
-                    console.log("Successfully updated the inventory!");
-                    console.log("You added " + parseInt(answer.quantity) + " units to " +
-                    chosenItem.product_name + "\n");
-
-                    menu();
                 }
-            )
-        })
+
+                var newQuantity = chosenItem.stock_quantity + parseInt(answer.quantity);
+                connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: newQuantity
+                        },
+                        {
+                            id: chosenItem.id
+                        }
+                    ],
+                    function (error) {
+                        if (error) throw error;
+                        console.log("Successfully updated the inventory!");
+                        console.log("You added " + parseInt(answer.quantity) + " units to " +
+                            chosenItem.product_name + "\n");
+
+                        menu();
+                    }
+                )
+            })
+    })
+
+}
+
+function addNewProduct() {
+    console.log("\nInserting a new product...\n")
+
+    inquirer.prompt([
+        {
+            name: "product_name",
+            message: "What is the products name?",
+        },
+        {
+            name: "department_name",
+            message: "What department will it be in?"
+        },
+        {
+            name: "price",
+            message: "How much does it cost?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            name: "stock_quantity",
+            message: "How many units do you have of this item?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function (answer) {
+        console.log("\n");
+
+        console.log("Adding " + answer.product_name + " to the database\n");
+
+        connection.query(
+            "INSERT INTO products SET ?",
+            {
+                product_name: answer.product_name,
+                department_name: answer.department_name,
+                price: answer.price,
+                stock_quantity: answer.stock_quantity
+            },
+            function(err, res){
+                if (err) throw err;
+
+                console.log(res.affectedRows + " product inserted!\n");
+                menu();
+
+            }
+        )
     })
 
 }
