63f5133defa5a5e282e0988a07e621b1e384b296
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-04-04 16:23:47 -0500

    update1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Bamazon
-Amazon counterpart
+Bamazon is an "Amazon" counter part that simulates what it is like to be a customer and what it's like to be a manager. The customer portion allows the user to see a series of items for sale along with their 
+
+* Department
+* Price
+* Amount in stock
+
+As soon as the program opens, we make a connection to the 
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,11 @@
+//these are the required npm packages we will
+//use throughout our program
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 const cTable = require("console.table");
 var chalk = require("chalk");
 
-
+//make a connection to the database
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -15,31 +17,40 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
+//when we connect
 connection.connect(function(err) {
     if (err) throw err;
 
+    //run the show items function
     showItems();
 })
 
+//show items function to render items in the database
 function showItems() {
     console.log(chalk.cyan("\nDisplaying all products...\n"));
+    //connect to the databse with a query to select all items
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
 
+        //render the results in a formatted table
         console.table(res);
+        //callback to our menu function
         menu();
     })
 
 }
 
+
+//menu function allows for choices and what the use would like to do
 function menu() {
     connection.query("SELECT * FROM products", function(err, results){
         if (err) throw err;
 
+        //inquirer asks for specific information
         inquirer
         .prompt([
             {
-                // * The first should ask them the ID of the product they would like to buy
+                // The first question should ask them the ID of the product they would like to buy
                 name: "id",
                 message: "Enter the ID number of the item you wish to buy.",
                 validate: function(value) {
@@ -50,7 +61,7 @@ function menu() {
                 }
             },
             {
-                // * The second message should ask how many units of the product they would like to buy
+                // The second message should ask how many units of the product they would like to buy
                 name: "quantity",
                 message: "How many would you like to buy?",
                 validate: function(value) {
@@ -61,8 +72,10 @@ function menu() {
                 }
             }
         ])
+        //then we want to use this information
         .then(function(answer){
             console.log("\n");
+            //this is where we assign a variable to the item the user has chosen 
             var chosenItem;
             for (var i = 0; i < results.length; i++) {
                 if (results[i].id === parseInt(answer.id)) {
@@ -70,9 +83,15 @@ function menu() {
                 }
             }
 
+            //if the amoun the user wants is less than 
+            //how much we have in stock, we can update our 
+            //data in the database
             if (parseInt(answer.quantity) <= chosenItem.stock_quantity) {
+                //the new quantity will be calculated
                 var newQuantity = chosenItem.stock_quantity - parseInt(answer.quantity);
+                //connect back to the database
                 connection.query(
+                    //what we want to update
                     "UPDATE products SET ? WHERE ?",
                     [
                         {
@@ -84,18 +103,26 @@ function menu() {
                     ],
                     function(error) {
                         if (error) throw error;
+
+                        //tell them it was a success and show how much spent
                         console.log("Purchase was " + chalk.green("successful!"));
                         var totalSpent = parseInt(answer.quantity) * parseInt(chosenItem.price)
                         console.log("You spent " + chalk.yellow("$" + totalSpent + "\n"));
+
+                        //then we ask the user if they would like to make
+                        //another purchsae or if they wish to exit
                         inquirer.prompt({
                             name: "continue",
                             type: "list",
                             message: "Would you like to [purchase] again or [exit]?",
                             choices: ["PURCHASE", "EXIT"]
                         }).then(function(answer){
+                            //if they want to make another purchase, 
+                            //take them  back to the show items 
                             if (answer.continue === "PURCHASE") {
                                 showItems();
                             }
+                            //otherwise end the connection 
                             else if(answer.continue === "EXIT") {
                                 connection.end();
                             }
@@ -104,6 +131,8 @@ function menu() {
                     }
                 );
             }
+            //otherwise we don't have enough so 
+            //ask them to try again or they can leave if they'd like
             else {
                 console.log(chalk.red("Insufficient Quantity!\n"));
                 inquirer.prompt({
